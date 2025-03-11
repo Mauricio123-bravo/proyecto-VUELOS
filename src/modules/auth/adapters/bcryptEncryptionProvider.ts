b5e2b5b90cf3f68e509d5e6991c3664f7523d67e
santiagohm <hermolsan@gmail.com>
2025-03-11 14:39:42 -0500

    Changed bcrypt for bcryptjs

--- src/modules/auth/adapters/bcryptEncryptionProvider.ts
+++ src/modules/auth/adapters/bcryptEncryptionProvider.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from "bcrypt";
+import * as bcrypt from "bcryptjs";
 import { EncryptionProvider } from "../models/providers/encryptionProvider";
 
 export default class BcryptProvider implements EncryptionProvider {
