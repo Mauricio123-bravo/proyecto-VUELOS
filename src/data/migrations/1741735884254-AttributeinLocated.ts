271e7d1df704f2e4582a787b03dbca1d9d83b3fb
santiagohm <hermolsan@gmail.com>
2025-03-15 14:01:25 -0500

    fixed migration

--- src/data/migrations/1741735884254-AttributeinLocated.ts
+++ src/data/migrations/1741735884254-AttributeinLocated.ts
@@ -4,13 +4,11 @@ export class AttributeinLocated1741735884254 implements MigrationInterface {
     name = 'AttributeinLocated1741735884254'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "located" DROP COLUMN "name"`);
         await queryRunner.query(`ALTER TABLE "located" ADD "name" character varying NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "located" DROP COLUMN "name"`);
-        await queryRunner.query(`ALTER TABLE "located" ADD "name" double precision NOT NULL`);
     }
 
 }
