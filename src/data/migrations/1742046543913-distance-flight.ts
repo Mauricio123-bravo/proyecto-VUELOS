e9389cef9d42c0af71b9fe98f33e1cfd7c6287fc
santiagohm <hermolsan@gmail.com>
2025-03-15 14:54:01 -0500

    fixed migration name airplanes

--- src/data/migrations/1742046543913-distance-flight.ts
+++ src/data/migrations/1742046543913-distance-flight.ts
@@ -4,13 +4,13 @@ export class DistanceFlight1742046543913 implements MigrationInterface {
     name = 'DistanceFlight1742046543913'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "flights" DROP COLUMN "distance"`);
-        await queryRunner.query(`ALTER TABLE "flights" ADD "distance" double precision NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "flights" ADD "distance" double precision`);
+        await queryRunner.query(`ALTER TABLE "airplanes" ADD "name" character varying(80)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "flights" DROP COLUMN "distance"`);
-        await queryRunner.query(`ALTER TABLE "flights" ADD "distance" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "airplanes" DROP COLUMN "name"`);
     }
 
 }
